93f6c4af94e129bd79b961e8cfbba6f5a9d00c08
suhail <suhail@example.com>
2014-09-16 18:03:33 +0700

    Added all java files
    Filter out of .idea/
    
    Update java from 1.6 to 1.8
    
    Remove comment
    
    Update readme file
    
    Add test folder for maven project
    
    Add new file
    
    Add test case
    
    Add web tier
    
    Demo js
    
    Update README.md
    
    Update test case and add apache maven
    
    Add test case with robotframework
    
    Update README.md
    
    Filter data
    
    Update example
    
    Update hello.html
    
    Update hello.html
    
    Update hello.html
    
    Bump mysql-connector-java from 6.0.6 to 8.0.28
    
    Bumps [mysql-connector-java](https://github.com/mysql/mysql-connector-j) from 6.0.6 to 8.0.28.
    - [Release notes](https://github.com/mysql/mysql-connector-j/releases)
    - [Changelog](https://github.com/mysql/mysql-connector-j/blob/release/8.0/CHANGES)
    - [Commits](https://github.com/mysql/mysql-connector-j/compare/6.0.6...8.0.28)
    
    ---
    updated-dependencies:
    - dependency-name: mysql:mysql-connector-java
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump junit from 4.12 to 4.13.1
    
    Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
    - [Release notes](https://github.com/junit-team/junit4/releases)
    - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
    - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Adding Dockerfile for RHEL Image
    
    jenkins file added
    
    Dockerfile

--- src/main/java/api/servlet/HelloServlet.java
+++ src/main/java/api/servlet/HelloServlet.java
@@ -13,40 +13,69 @@ import org.codehaus.jackson.map.ObjectMapper;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// resp.getWriter().print("Hello from Servlet 3");
-		
-		User user = new User();
-		user.setId("1");
-		user.setName("Up1");
-		
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.writeValue(resp.getOutputStream(), user);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response)
+            throws ServletException, IOException {
 
-	}
+        // Business Logic
+        UserModel model = new UserModel();
+        User user = model.getUser();
+
+//        user.setId("1111");
+//        user.setName("Up1");
+
+        // Create response message
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), user);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Convert request to Java object
+        ObjectMapper mapper = new ObjectMapper();
+        User sendUser = mapper.readValue(req.getInputStream(), User.class);
+
+        // Business Logic
+        User user = new User("");
+        user.setId("0000");
+        user.setName("Response from POST form " + sendUser.getName());
+
+        // Create response message
+        mapper.writeValue(response.getOutputStream(), user);
+
+    }
 
 }
 
 class User {
-	private String id;
-	private String name;
+    private String id;
+    private String name;
 
-	public String getId() {
-		return id;
-	}
+    public User(String id) {
+        this.id = id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
